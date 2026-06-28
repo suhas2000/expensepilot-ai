18750f80c964443bfb2f1e9f2d503c494469ecdf
suhas2000 <suhaspjoshi2000@gmail.com>
2026-06-28 17:26:13 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/suhas/expensepilot/ExpensepilotAiApplication.java
@@ -0,0 +1,13 @@
+package com.suhas.expensepilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensepilotAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensepilotAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suhas/expensepilot/ExpensepilotAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suhas.expensepilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensepilotAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
